914f9257219d97f98e57b7d21e57b8801bc622e1
Marun <marin.maulbon@gmail.com>
2026-05-05 12:19:27 +0200

    create new media in assets when uploading media

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"os"
+	"mime"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +37,59 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "the given id is not valid", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "video not found", err)
+		return
+	}
+	if video.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "this is not your video", err)
+		return
+	}
+	mediatype, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "the header is incorrect", err)
+		return
+	}
+	splitedContentType := strings.Split(mediatype, "/")
+	fileExtension := splitedContentType[1]
+	if fileExtension != "jpeg" && fileExtension != "png" {
+		respondWithError(w, http.StatusBadRequest, "not allowed to upload this file type", err)
+		return
+	}
+	thumbnailFile := fmt.Sprintf("%s.%s", videoIDString, fileExtension)
+	thumbnailFilePath := filepath.Join(cfg.assetsRoot, thumbnailFile)
+	newFile, err := os.Create(thumbnailFilePath)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "something went wrong while creating", err)
+		return
+	}
+
+	if _, err := io.Copy(newFile, file); err != nil {
+		respondWithError(w, http.StatusBadRequest, "something went wrong while copying", err)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, thumbnailFile)
+	video.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "something went wrong while creating", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
 }
